6af1cd165c997ac775b3effe20c563c183b61854
teeweizhong10 <teeweizhong10@gmail.com>
2021-11-09 22:23:36 -0600

    Test processData print

--- processData.py
+++ processData.py
@@ -112,13 +112,13 @@ def read_user_data(uuid):
 uuid = '1155FF54-63D3-4AB2-9863-8385D0BD0A13';
 (X,Y,M,timestamps,feature_names,label_names) = read_user_data(uuid);
 
-'''
-TESTING: Prints user data
+
+#TESTING: Prints user data
 print( "The parts of the user's data (and their dimensions):");
 print ("Every example has its timestamp, indicating the minute when the example was recorded");
 print ("User %s has %d examples (~%d minutes of behavior)" % (uuid,len(timestamps),len(timestamps)));
 timestamps.shape
-'''
+
 
 
 
